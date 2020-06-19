e86fd71d18e05d709017089cce4d16a1208522fd
Serj Sidelnikov <serj2107@gmail.com>
2020-06-19 11:33:10 +0300

    Module3-task1

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,21 +1,100 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import WelcomeScreen from '../welcome-screen/welcome-screen';
+import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen';
+import GenreQuestionScreen from '../genre-question-screen/genre-question-screen';
+import {GameType} from '../../const';
 
-const welcomeButtonHandler = () => {};
+class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
 
-const App = ({errorsCount}) => {
-  return (
-    <WelcomeScreen
-      errorsCount={errorsCount}
-      onWelcomeButtonClick={welcomeButtonHandler}
-    />
-  );
-};
+    this.state = {
+      step: -1,
+    };
+  }
+
+  _renderGameScreen() {
+    const {errorsCount, questions} = this.props;
+    const {step} = this.state;
+    const question = questions[step];
+
+    if (step === -1 || step >= questions.length) {
+      return (
+        <WelcomeScreen
+          errorsCount={errorsCount}
+          onWelcomeButtonClick={() => {
+            this.setState({
+              step: 0
+            });
+          }}
+        />
+      );
+    }
+
+    if (question) {
+      switch (question.type) {
+        case GameType.ARTIST:
+          return (
+            <ArtistQuestionScreen
+              question={question}
+              onAnswer={() => {
+                this.setState((prevState) => ({
+                  step: prevState.step + 1,
+                }));
+              }}
+            />
+          );
+
+        case GameType.GENRE:
+          return (
+            <GenreQuestionScreen
+              question={question}
+              onAnswer={() => {
+                this.setState((prevState) => ({
+                  step: prevState.step + 1,
+                }));
+              }}
+            />
+          );
+      }
+    }
+
+    return null;
+  }
+
+  render() {
+    const {questions} = this.props;
+
+    return (
+      <Router>
+        <Switch>
+          <Route exact path={`/`}>
+            {this._renderGameScreen()}
+          </Route>
+          <Route exact path={`/artist`}>
+            <ArtistQuestionScreen
+              question={questions[1]}
+              onAnswer={() => {}}
+            />
+          </Route>
+          <Route exact path={`/genre`}>
+            <GenreQuestionScreen
+              question={questions[0]}
+              onAnswer={() => {}}
+            />
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
+}
 
 App.propTypes = {
   errorsCount: PropTypes.number.isRequired,
+  questions: PropTypes.array.isRequired,
 };
 
 export default App;
---
+++ src/components/artist-question-screen/artist-question-screen.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Header from '../header/header';
+import {GameType} from '../../const';
+
+const ArtistQuestionScreen = ({question, onAnswer}) => {
+  const {answers, song} = question;
+
+  return (
+    <section className="game game--artist">
+      <Header/>
+
+      <section className="game__screen">
+        <h2 className="game__title">Кто исполняет эту песню?</h2>
+        <div className="game__track">
+          <div className="track">
+            <button className="track__button track__button--play" type="button"/>
+            <div className="track__status">
+              <audio
+                src={song.src}
+              />
+            </div>
+          </div>
+        </div>
+
+        <form className="game__artist">
+          {answers.map((answer, i) => (
+            <div className="artist" key={answer.artist}>
+              <input
+                className="artist__input visually-hidden"
+                type="radio"
+                name="answer"
+                value={`answer-${i}`}
+                id={`answer-${i}`}
+                onChange={(evt) => {
+                  evt.preventDefault();
+                  onAnswer(question, answer);
+                }}
+              />
+              <label className="artist__name" htmlFor={`answer-${i}`}>
+                <img
+                  className="artist__picture"
+                  src={answer.picture}
+                  alt={answer.artist}
+                />
+                {answer.artist}
+              </label>
+            </div>
+          ))}
+        </form>
+      </section>
+    </section>
+  );
+};
+
+ArtistQuestionScreen.propTypes = {
+  question: PropTypes.shape({
+    answers: PropTypes.arrayOf(
+        PropTypes.shape({
+          artist: PropTypes.string.isRequired,
+          picture: PropTypes.string.isRequired,
+        })
+    ).isRequired,
+    song: PropTypes.shape({
+      artist: PropTypes.string.isRequired,
+      src: PropTypes.string.isRequired,
+    }).isRequired,
+    type: PropTypes.oneOf([GameType.ARTIST, GameType.GENRE]).isRequired,
+  }).isRequired,
+  onAnswer: PropTypes.func.isRequired,
+};
+
+export default ArtistQuestionScreen;
